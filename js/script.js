15ea56772f8629b562c5fa4f96f151c3dff7aa44
tylerconn <85835961+tylerconn@users.noreply.github.com>
2022-01-30 01:46:25 -0500

    added js and updated fonts/css

---
+++ js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    console.log( "ready!" );
+    $(window).scroll(function() {
+        var scroll = $(window).scrollTop();
+        if (scroll >= 1) {
+            $('.arrow').addClass('fade');
+        } else{
+            $('.arrow').removeClass('fade');
+        }
+    })
+});
