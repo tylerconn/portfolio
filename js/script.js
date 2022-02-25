ddfd4678bd6120fc2e3c4f7d3c5871d4bab1f132
tylerconn <85835961+tylerconn@users.noreply.github.com>
2022-02-24 23:42:15 -0500

    added jQuery for scrolling

--- js/script.js
+++ js/script.js
@@ -8,3 +8,27 @@ $(document).ready(function() {
         }
     })
 });
+//Go To Top Button Fades
+$(document).scroll(function() {
+    var y = $(this).scrollTop();
+    if (y > 50) {
+      $('.gototop').fadeIn();
+    } else {
+      $('.gototop').fadeOut();
+    }
+});
+//Go To Top Button Action
+$(document).ready(function(){
+    $('.gototop').click(function(){
+        $(window).scrollTop("body");
+    });
+});
+
+$(document).scroll(function() {
+    var scroll = $(this).scrollTop();
+    if (scroll > 0) {
+        $('#scrollDown').fadeOut();
+    } else {
+        $('#scrollDown').fadeIn();
+    }
+});
